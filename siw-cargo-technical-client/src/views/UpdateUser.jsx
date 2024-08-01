873a3bd83fabc362bee2b47287203fd57ea46e9f
nahunicoR <nahunico.r@gmail.com>
2024-07-31 23:56:54 -0300

    fix:uncontrolled formulary

--- siw-cargo-technical-client/src/views/UpdateUser.jsx
+++ siw-cargo-technical-client/src/views/UpdateUser.jsx
@@ -7,19 +7,65 @@ import { useForm } from '../hook/useForm'
 import { updateUser } from '../services/services';
 import { useNavigate } from 'react-router-dom';
 
+const validate = (input) => {
+    let errors = {};
 
+    if (!input.nombre) {
+        errors.nombre = 'Nombre requerido';
+    }
+
+    if (!input.username) {
+        errors.username = 'Usuario requerido';
+    }
+
+    if (!input.oldPassword) {
+        errors.oldPassword = 'Contraseña requerida';
+    }
+    if (input.oldPassword !== input.newPassword) {
+        errors.newPassword = 'Las contraseñas no coinciden';
+    }
+    return errors;
+};
 
 const UpdateUser = () => {
-    const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
+    const [loading, setLoading] = useState(false)
 
-    const { nombre, username, oldPassword, newPassword, onInputChange, onResetForm, error } = useForm({
-        nombre: '',
-        username: '',
-        oldPassword: '',
-        newPassword: '',
+    const [input, setInput] = useState({
+        nombre: "",
+        username: "",
+        oldPassword: "",
+        newPassword: "",
+    });
+    const [errors, setErrors] = useState({
+        nombre: "",
+        username: "",
+        oldPassword: "",
+        newPassword: "",
     });
 
+    const handleInput = (e) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value,
+        });
+        setErrors(
+            validate({
+                ...input,
+                [e.target.name]: e.target.value,
+            })
+        );
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const data = await updateUser(input);
+            navigate('/');
+        } catch (error) {
+            navigate('/');
+        }
+    }
     useEffect(() => {
         setLoading(true)
         setTimeout(() => {
@@ -28,59 +74,52 @@ const UpdateUser = () => {
     }, [])
 
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const data = await updateUser({ nombre, username, oldPassword, newPassword });
-            navigate('/');
-            onResetForm();
-            console.log(data);
-        } catch (error) {
-            console.log({ error });
-        }
-    }
-
-
 
     return (
         <>
-            {
+            {/* {
                 loading ? (
                     <Center h='100vh'>
                         <CircularProgress isIndeterminate color='green.300' />
-                    </Center>
-                ) : <Box
-                    width={"100vw"}
-                    height="100vh"
-                    backgroundImage={image}
-                    backdropBlur={"px"}
-                    backgroundPosition="center"
-                    backgroundRepeat="no-repeat"
-                    backgroundSize="cover"
-                    display={"flex"}
-                    justifyContent="center"
-                    alignItems="center"
-                >
-                    <Container
-                        width={"50%"}
-                        height="100vh">
-                        <Card
-                            width={"100%"}
-                            height={"80%"}
+                    </Center> */}
+            {/* ) :  */}
+            <Box
+                width={"100vw"}
+                height="100vh"
+                backgroundImage={image}
+                backdropBlur={"px"}
+                backgroundPosition="center"
+                backgroundRepeat="no-repeat"
+                backgroundSize="cover"
+                display={"flex"}
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Container
+                    width={"60%"}
+                    height="100vh">
+                    <Card
+                        width={"100%"}
+                        height={"90%"}
+                        display={"flex"}
+                        justifyContent="center"
+                        alignItems="center"
+                        bg="gray.100"
+                        borderRadius={10}
+                        m={5}
+                        gap={5}
+                        textAlign={"center"}
+                    >
+                        <Text fontSize="3xl">
+                            Actualiza tu Contraseña
+                        </Text>
+                        <Box
                             display={"flex"}
                             justifyContent="center"
                             alignItems="center"
-                            bg="white"
-                            variant="outline"
-                            boxShadow="dark-lg"
-                            p={10}
-                            borderRadius={10}
-                            m={5}
+                            flexDirection="column"
                             gap={5}
                         >
-                            <Text fontSize="3xl">
-                                Actualiza tu Contraseña
-                            </Text>
                             <form onSubmit={handleSubmit}>
                                 <FormControl >
                                     <FormLabel
@@ -88,14 +127,16 @@ const UpdateUser = () => {
                                     >
                                         Nombre
                                     </FormLabel>
-                                    <Input type='text' name={'nombre'} id='nombre' value={nombre} onChange={onInputChange} />
+                                    <Input type='text' name={'nombre'} id='nombre' value={input.nombre} onChange={handleInput} />
+                                    {errors.nombre && <Text color={"red.500"} fontSize={'sm'}>{errors.nombre}</Text>}
                                 </FormControl>
                                 <FormControl >
                                     <FormLabel
                                         htmlFor='username'
                                     >
                                         Usuario</FormLabel>
-                                    <Input type='text' name={'username'} id='username' value={username} onChange={onInputChange} />
+                                    <Input type='text' name={'username'} id='username' value={input.username} onChange={handleInput} />
+                                    {errors.username && <Text color={"red.500"} fontSize={'sm'}>{errors.username}</Text>}
                                 </FormControl>
                                 <FormControl >
                                     <FormLabel
@@ -105,7 +146,8 @@ const UpdateUser = () => {
                                     </FormLabel>
                                 </FormControl>
                                 <FormControl >
-                                    <Input type='password' name={'oldPassword'} id='oldPassword' value={oldPassword} onChange={onInputChange} />
+                                    <Input type='password' name={'oldPassword'} id='oldPassword' value={input.oldPassword} onChange={handleInput} />
+                                    {errors.oldPassword && <Text color={"red.500"} fontSize={'sm'}>{errors.oldPassword}</Text>}
                                 </FormControl>
                                 <FormControl >
                                     <FormLabel
@@ -113,24 +155,27 @@ const UpdateUser = () => {
                                     >
                                         Nueva Contraseña
                                     </FormLabel>
-                                    <Input type='password' name={'newPassword'} id='newPassword' value={newPassword} onChange={onInputChange} />
-                                    <Button
-                                        type='submit'
-                                        colorScheme="blue"
-                                        variant="solid"
-                                        width={"50%"}
-                                        height={"100%"}
-                                        ml={20}
-                                        onClick={() => { }}
-                                    >
-                                        Actualizar
-                                    </Button>
+                                    <Input type='password' name={'newPassword'} id='newPassword' value={input.newPassword} onChange={handleInput} />
+                                    {errors.newPassword && <Text color={"red.500"} fontSize={'sm'}>{errors.newPassword}</Text>}
                                 </FormControl>
+
+
+                                <Button
+                                    type='submit'
+                                    colorScheme="blue"
+                                    variant="solid"
+                                    width={"50%"}
+                                    height={"10%"}
+                                    mt={10}
+                                >
+                                    Actualizar
+                                </Button>
                             </form>
-                        </Card>
-                    </Container>
-                </Box>
-            }
+                        </Box>
+                    </Card>
+                </Container>
+            </Box >
+            {/* } */}
 
         </>
 
